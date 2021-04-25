7eb88bd1aacd11fc9b0d823b9726dc5171d450a1
Yakshchopra <yakshchopra@gmail.com>
2021-04-25 11:49:06 +0530

    🐛 fen corection

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public')); 
+app.use(express.static('public'));
 var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-http.listen(port, function() {
-    console.log('listening on *: ' + port);
+http.listen(port, function () {
+  console.log('listening on port: ' + port);
 });
 
 var io = require('socket.io')(http);
- 
-io.on('connection', function(socket) {
-    console.log('new connection');
 
-    // Called when the client calls socket.emit('move')
-    socket.on('move', function(msg) {
-       socket.broadcast.emit('move', msg); 
-    });
+io.on('connection', function (socket) {
+  console.log('new connection');
+
+  // Called when the client calls socket.emit('move')
+  socket.on('move', function (msg) {
+    socket.broadcast.emit('move', msg);
+  });
 });
-
